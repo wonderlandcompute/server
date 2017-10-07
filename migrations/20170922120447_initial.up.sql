c3069f7548087958d60075f26b169e0b229f93aa
andreiSaw <isaevnextdoor@gmail.com>
2017-10-07 16:25:16 +0300

    andrei

--- migrations/20170922120447_initial.up.sql
+++ migrations/20170922120447_initial.up.sql
@@ -1,16 +1,21 @@
 CREATE TABLE points (
-    id              SERIAL NOT NULL,
-    project         VARCHAR(40),
-    status          SMALLINT,
+  id           SERIAL NOT NULL,
+  project      VARCHAR(40),
+  status       SMALLINT,
 
-    coordinate      TEXT NOT NULL DEFAULT '',
-    metric_value    TEXT NOT NULL DEFAULT '',
-    metadata        TEXT NOT NULL DEFAULT '',
+  coordinate   TEXT   NOT NULL             DEFAULT '',
+  metric_value TEXT   NOT NULL             DEFAULT '',
+  metadata     TEXT   NOT NULL             DEFAULT '',
 
-    created         timestamp without time zone default (now() at time zone 'utc'),
-    creator         VARCHAR(40),
+  created      TIMESTAMP WITHOUT TIME ZONE DEFAULT (now() AT TIME ZONE 'utc'),
+  creator      VARCHAR(40),
 
-    PRIMARY KEY (project, id)
+  input        TEXT   NOT NULL             DEFAULT '',
+  output       TEXT   NOT NULL             DEFAULT '',
+  kind         SMALLINT,
+
+  PRIMARY KEY (project, id)
 );
 
-CREATE INDEX status_idx ON points (status);
+CREATE INDEX status_idx
+  ON points (status);
