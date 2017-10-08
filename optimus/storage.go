d7fee12a4f9a94b974b83b9423b274a460be41f1
andreiSaw <isaevnextdoor@gmail.com>
2017-10-08 19:24:32 +0300

    tabs to 4 spaces

--- optimus/storage.go
+++ optimus/storage.go
@@ -1,189 +1,191 @@
 package optimus
 
 import (
-	"database/sql"
-	_ "github.com/lib/pq"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "os/user"
 )
 
 type OptimusStorageConfig struct {
-	DatabaseURI string `json:"db_uri"`
+    DatabaseURI string `json:"db_uri"`
 }
 
 type OptimusStorage struct {
-	db     *sql.DB
-	Config OptimusStorageConfig
+    db     *sql.DB
+    Config OptimusStorageConfig
 }
 
 func NewOptimusStorage(db_uri string) (*OptimusStorage, error) {
-	ret := &OptimusStorage{
-		Config: OptimusStorageConfig{DatabaseURI: db_uri},
-	}
+    ret := &OptimusStorage{
+        Config: OptimusStorageConfig{DatabaseURI: db_uri},
+    }
 
-	err := ret.Connect()
+    err := ret.Connect()
 
-	return ret, err
+    return ret, err
 }
 
 func (storage *OptimusStorage) Connect() error {
-	db, err := sql.Open("postgres", storage.Config.DatabaseURI)
-	storage.db = db
-	return err
+    db, err := sql.Open("postgres", storage.Config.DatabaseURI)
+    storage.db = db
+    return err
 }
 
 func (storage *OptimusStorage) CreateJob(job *Job, creator User) (*Job, error) {
-	tx, err := storage.db.Begin()
-	if err != nil {
-		return nil, err
-	}
+    tx, err := storage.db.Begin()
+    if err != nil {
+        return nil, err
+    }
 
-	created_job := &Job{}
-	err = tx.QueryRow(`
+    created_job := &Job{}
+    err = tx.QueryRow(`
 		INSERT INTO jobs (project, status, coordinate, metric_value, metadata, creator, input, output, kind)
 		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
 		RETURNING id, project, status, coordinate, metric_value, metadata, input, output, kind;`,
-		job.Project, job.Status, job.Coordinate, job.MetricValue, job.Metadata,
-		creator.Username, job.Input, job.Output, job.Kind,
-	).Scan(
-		&created_job.Id,
-		&created_job.Project,
-		&created_job.Status,
-		&created_job.Coordinate,
-		&created_job.MetricValue,
-		&created_job.Metadata,
-		&created_job.Input,
-		&created_job.Output,
-		&created_job.Kind,
-	)
-	if err != nil {
-		return nil, err
-	}
+        job.Project, job.Status, job.Coordinate, job.MetricValue, job.Metadata,
+        creator.Username, job.Input, job.Output, job.Kind,
+    ).Scan(
+        &created_job.Id,
+        &created_job.Project,
+        &created_job.Status,
+        &created_job.Coordinate,
+        &created_job.MetricValue,
+        &created_job.Metadata,
+        &created_job.Input,
+        &created_job.Output,
+        &created_job.Kind,
+    )
+    if err != nil {
+        return nil, err
+    }
 
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-	}
-	return created_job, err
+    err = tx.Commit()
+    if err != nil {
+        tx.Rollback()
+    }
+    return created_job, err
 }
 
-func (storage *OptimusStorage) CreateMultipleJobs(jobs []*Job, creator User) ([]*Job, error) {
-	tx, err := storage.db.Begin()
-	if err != nil {
-		return nil, err
-	}
-	list_jobs := []*Job{}
-	n := len(jobs)
-	for i := 0; i < n; i += 1 {
-		created_job := &Job{}
-		job := jobs[i]
-		err = tx.QueryRow(`
+func (storage *OptimusStorage) CreateMultipleJobs(jobs []*Job, creator User, project string) ([]*Job, error) {
+    tx, err := storage.db.Begin()
+    if err != nil {
+        return nil, err
+    }
+    list_jobs := []*Job{}
+    n := len(jobs)
+    for i := 0; i < n; i += 1 {
+        created_job := &Job{}
+        job := jobs[i]
+        job.Project = project
+        err = tx.QueryRow(`
 			INSERT INTO jobs (project, status, coordinate, metric_value, metadata, input, output, kind, creator)
 			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
 			RETURNING id, project, status, coordinate, metric_value,metadata, input, output, kind;`,
-			job.Project, job.Status, job.Coordinate, job.MetricValue, job.Metadata, job.Input, job.Output, job.Kind,
-			creator.Username,
-		).Scan(
-			&created_job.Id,
-			&created_job.Project,
-			&created_job.Status,
-			&created_job.Coordinate,
-			&created_job.MetricValue,
-			&created_job.Metadata,
-			&created_job.Input,
-			&created_job.Output,
-			&created_job.Kind,
-		)
-		if err != nil {
-			return nil, err
-		}
-		list_jobs = append(list_jobs, created_job)
-	}
+            job.Project, job.Status, job.Coordinate, job.MetricValue, job.Metadata, job.Input, job.Output, job.Kind,
+            creator.Username,
+        ).Scan(
+            &created_job.Id,
+            &created_job.Project,
+            &created_job.Status,
+            &created_job.Coordinate,
+            &created_job.MetricValue,
+            &created_job.Metadata,
+            &created_job.Input,
+            &created_job.Output,
+            &created_job.Kind,
+        )
+        if err != nil {
+            return nil, err
+        }
+        list_jobs = append(list_jobs, created_job)
+    }
 
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-	}
-	return list_jobs, err
+    err = tx.Commit()
+    if err != nil {
+        tx.Rollback()
+    }
+    return list_jobs, err
 }
 
 func (storage *OptimusStorage) GetJob(id uint64, project string) (*Job, error) {
-	tx, err := storage.db.Begin()
-	if err != nil {
-		return nil, err
-	}
+    tx, err := storage.db.Begin()
+    if err != nil {
+        return nil, err
+    }
 
-	job := &Job{}
-	err = tx.QueryRow(`
+    job := &Job{}
+    err = tx.QueryRow(`
 		SELECT id, project, status, coordinate, metric_value, metadata, input, output, kind FROM jobs
 		WHERE id=$1 AND project=$2;`,
-		id, project,
-	).Scan(
-		&job.Id,
-		&job.Project,
-		&job.Status,
-		&job.Coordinate,
-		&job.MetricValue,
-		&job.Metadata,
-		&job.Input,
-		&job.Output,
-		&job.Kind,
-	)
-	if err != nil {
-		return nil, err
-	}
+        id, project,
+    ).Scan(
+        &job.Id,
+        &job.Project,
+        &job.Status,
+        &job.Coordinate,
+        &job.MetricValue,
+        &job.Metadata,
+        &job.Input,
+        &job.Output,
+        &job.Kind,
+    )
+    if err != nil {
+        return nil, err
+    }
 
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-	}
-	return job, err
+    err = tx.Commit()
+    if err != nil {
+        tx.Rollback()
+    }
+    return job, err
 }
 
 func (storage *OptimusStorage) ListJobs(project string) (*ListOfJobs, error) {
-	query := `SELECT id, project, status, coordinate, metric_value, metadata, input, output, kind
+    query := `SELECT id, project, status, coordinate, metric_value, metadata, input, output, kind
 		FROM jobs
 		WHERE project=$1;`
 
-	rows, err := storage.db.Query(query, project)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+    rows, err := storage.db.Query(query, project)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
 
-	ret := &ListOfJobs{Jobs: []*Job{}}
+    ret := &ListOfJobs{Jobs: []*Job{}}
 
-	for rows.Next() {
-		job := &Job{}
+    for rows.Next() {
+        job := &Job{}
 
-		err = rows.Scan(
-			&job.Id,
-			&job.Project,
-			&job.Status,
-			&job.Coordinate,
-			&job.MetricValue,
-			&job.Metadata,
-			&job.Input,
-			&job.Output,
-			&job.Kind,
-		)
+        err = rows.Scan(
+            &job.Id,
+            &job.Project,
+            &job.Status,
+            &job.Coordinate,
+            &job.MetricValue,
+            &job.Metadata,
+            &job.Input,
+            &job.Output,
+            &job.Kind,
+        )
 
-		if err != nil {
-			return nil, err
-		}
-		ret.Jobs = append(ret.Jobs, job)
-	}
+        if err != nil {
+            return nil, err
+        }
+        ret.Jobs = append(ret.Jobs, job)
+    }
 
-	err = rows.Err()
-	return ret, err
+    err = rows.Err()
+    return ret, err
 }
 
 func (storage *OptimusStorage) UpdateJob(job *Job) (*Job, error) {
-	tx, err := storage.db.Begin()
-	if err != nil {
-		return nil, err
-	}
+    tx, err := storage.db.Begin()
+    if err != nil {
+        return nil, err
+    }
 
-	result_job := &Job{}
-	err = tx.QueryRow(`
+    result_job := &Job{}
+    err = tx.QueryRow(`
 		UPDATE jobs
 		SET
 			status=$1,
@@ -194,45 +196,45 @@ func (storage *OptimusStorage) UpdateJob(job *Job) (*Job, error) {
 			kind=$6
 		WHERE id=$7 and project=$8
 		RETURNING id, project, status, coordinate, metric_value, metadata, input, output, kind;`,
-		job.Status,
-		job.MetricValue,
-		job.Metadata,
-		job.Input,
-		job.Output,
-		job.Kind,
-		job.Id,
-		job.Project,
-	).Scan(
-		&result_job.Id,
-		&result_job.Project,
-		&result_job.Status,
-		&result_job.Coordinate,
-		&result_job.MetricValue,
-		&result_job.Metadata,
-		&result_job.Input,
-		&result_job.Output,
-		&result_job.Kind,
-	)
-	if err != nil {
-		return result_job, err
-	}
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-		return result_job, err
-	}
+        job.Status,
+        job.MetricValue,
+        job.Metadata,
+        job.Input,
+        job.Output,
+        job.Kind,
+        job.Id,
+        job.Project,
+    ).Scan(
+        &result_job.Id,
+        &result_job.Project,
+        &result_job.Status,
+        &result_job.Coordinate,
+        &result_job.MetricValue,
+        &result_job.Metadata,
+        &result_job.Input,
+        &result_job.Output,
+        &result_job.Kind,
+    )
+    if err != nil {
+        return result_job, err
+    }
+    err = tx.Commit()
+    if err != nil {
+        tx.Rollback()
+        return result_job, err
+    }
 
-	return result_job, err
+    return result_job, err
 }
 
 func (storage *OptimusStorage) PullJobs(how_many uint32) ([]*Job, error) {
-	tx, err := storage.db.Begin()
-	if err != nil {
-		return nil, err
-	}
+    tx, err := storage.db.Begin()
+    if err != nil {
+        return nil, err
+    }
 
-	result_jobs := []*Job{}
-	rows, err := tx.Query(`
+    result_jobs := []*Job{}
+    rows, err := tx.Query(`
 		WITH pulled_pts as (
 			SELECT id, project
 			FROM jobs
@@ -245,29 +247,29 @@ func (storage *OptimusStorage) PullJobs(how_many uint32) ([]*Job, error) {
 		FROM pulled_pts
 		WHERE pulled_pts.id=pts.id and pulled_pts.project=pts.project
 		RETURNING pts.id, pts.project, pts.status, pts.coordinate, pts.metric_value, pts.metadata, pts.input, pts.output, pts.kind;`,
-		Job_PENDING,
-		how_many,
-		Job_PULLED,
-	)
-	if err != nil {
-		return nil, err
-	}
+        Job_PENDING,
+        how_many,
+        Job_PULLED,
+    )
+    if err != nil {
+        return nil, err
+    }
 
-	for rows.Next() {
-		job := Job{}
-		err = rows.Scan(&job.Id, &job.Project, &job.Status, &job.Coordinate, &job.MetricValue, &job.Metadata, &job.Input, &job.Output, &job.Kind)
-		if err != nil {
-			return nil, err
-		}
+    for rows.Next() {
+        job := Job{}
+        err = rows.Scan(&job.Id, &job.Project, &job.Status, &job.Coordinate, &job.MetricValue, &job.Metadata, &job.Input, &job.Output, &job.Kind)
+        if err != nil {
+            return nil, err
+        }
 
-		result_jobs = append(result_jobs, &job)
-	}
+        result_jobs = append(result_jobs, &job)
+    }
 
-	err = tx.Commit()
-	if err != nil {
-		tx.Rollback()
-		return nil, err
-	}
+    err = tx.Commit()
+    if err != nil {
+        tx.Rollback()
+        return nil, err
+    }
 
-	return result_jobs, err
+    return result_jobs, err
 }
