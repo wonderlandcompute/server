947daa4ebdbfad18e373b6b97a44f4b16d24d22a
Sasha Baranov <sashab1@yandex-team.ru>
2017-10-13 15:57:18 +0300

    Fix pathes and docker build

--- disneyland_server.go
+++ disneyland_server.go
@@ -8,7 +8,7 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/sirupsen/logrus"
-	"gitlab.com/lambda-hse/optimus/disneyland"
+	"gitlab.com/lambda-hse/disneyland/disneyland"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
